cc4c2c4ecfdee55e661a17c6390abd8b2a08b7a8
devLIPEr <dev.develiper@gmail.com>
2023-10-12 21:41:50 -0300

    Added pdf view and hbs partial

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,9 +5,14 @@ export class AppController {
   @Get()
   @Render('index')
   root() {}
-
   
   @Get('/login')
   @Render('login')
   branchIndex() {}
+  
+  @Get('/pdf')
+  @Render('pdf')
+  branchPDF() {
+    return {materia: 'Engenharia de Software I', data: '12/02/2023', sala: 'Sala 1 Bloco 2', horario: '08:20 - 10:00'};
+  }
 }
