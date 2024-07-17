96746101ab252712fe0d2cc5b0765869c76f0262
Rafael-Rech <122633585+Rafael-Rech@users.noreply.github.com>
2024-07-17 18:55:36 -0300

    Corrected dateCheck.middleware.ts

--- src/middleware/repositionScheduling/dateCheck.middleware.ts
+++ src/middleware/repositionScheduling/dateCheck.middleware.ts
@@ -5,11 +5,14 @@ import { NextFunction, Request, Response } from "express";
 export default class DateCheckMiddleware implements NestMiddleware{
     use(req: Request, res: Response, next: NextFunction) {
         var date : string = req.body.date;
-        if(date != ''){
-            next();
-        } else {
+        var day = parseInt(date.split('/')[0]);
+        var month = parseInt(date.split('/')[1]);
+        var year = parseInt(date.split('/')[2]);
+        if(Number.isNaN(day)|| Number.isNaN(month) || Number.isNaN(year) || date == ''){
             res.status(400);
             res.send({"mensagem": "Insira uma data!"});
+        } else {
+            next();
         }
     }
 }
